80f06d5ad0143c35050b31a1052a290ebe524006
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2019-01-01 13:47:28 +0530

    Initial JS code
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Created by bhavyaagg on 01/01/19
+let gameArray = []
+let tdElements = []
+
+for (let i = 0; i < 16; i++) {
+  gameArray[i] = 0;
+  tdElements[i] = document.getElementById(`td${i}`)
+}
+
+let randomIndex = Math.floor(Math.random() * 16)
+gameArray[randomIndex] = 2;
+
+updateTable()
+
+
+function updateTable() {
+  for (let i = 0; i < 16; i++) {
+    tdElements[i].innerText = gameArray[i] === 0 ? "" : gameArray[i];
+  }
+}
